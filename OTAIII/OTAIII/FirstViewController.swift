417344fe49cf3543c760e44f02ca8de3838a9e5e
Todd Papke <sabulatodd@mac.com>
2016-10-17 12:51:28 -0500

    First compile

---
+++ OTAIII/OTAIII/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OTAIII
+//
+//  Created by Todd Papke on 10/17/16.
+//  Copyright © 2016 Todd Papke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OTAIII/OTAIII/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OTAIII
+//
+//  Created by Todd Papke on 10/17/16.
+//  Copyright © 2016 Todd Papke. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
